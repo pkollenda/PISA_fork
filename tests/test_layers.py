a6409d94177304071bd0d05e912e1daf954915ee
Joao Moura <operte@gmail.com>
2024-11-23 18:00:04 +0100

    remove fixtures

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -1,61 +1,51 @@
 import geopandas as gpd
-import pytest
 from shapely.geometry import MultiPolygon, Polygon
 
 from gpbp.layers import AdmArea
 
 
-@pytest.fixture
-def mock_country_gdf() -> gpd.GeoDataFrame:
-    """Create a level=0 administrative area (country) GeoDataFrame"""
-    # Create two simple square polygons
-    polygon1 = Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])
-    polygon2 = Polygon([(2, 2), (3, 2), (3, 3), (2, 3)])
-
-    # Combine them into a MultiPolygon
-    multipolygon = MultiPolygon([polygon1, polygon2])
-
-    data = {
-        'id': [0],
-        'COUNTRY': ["Mock Country"],
-        'geometry': [multipolygon],
-    }
-
-    gdf = gpd.GeoDataFrame(data, crs='EPSG:4326')
-
-    return gdf
-
-@pytest.fixture
-def mock_region_gdf():
-    """Create a level=1 administrative area (region) GeoDataFrame"""
-    # Create two simple square polygons
-    polygon1 = Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])
-    polygon2 = Polygon([(2, 2), (3, 2), (3, 3), (2, 3)])
-
-    # Combine them into a MultiPolygon
-    multipolygon = MultiPolygon([polygon1, polygon2])
-
-    data = {
-        'id': [0, 1],
-        'COUNTRY': ["Mock Country", "Mock Country"],
-        'NAME_1': ["Mock Region 1", "Mock Region 2"],
-        'geometry': [multipolygon, multipolygon],
-    }
-
-    gdf = gpd.GeoDataFrame(data, crs='EPSG:4326')
-
-    return gdf
-
 class TestAdmAreaGetCountryData:
-    def test_get_country_data_level_0(self, mocker, mock_country_gdf):
-        mocker.patch("layers.GADMDownloader.get_shape_data_by_country_name", return_value=mock_country_gdf)
+    def test_get_country_data_level_0(self, mocker):
+        # Create two simple square polygons
+        polygon1 = Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])
+        polygon2 = Polygon([(2, 2), (3, 2), (3, 3), (2, 3)])
+
+        # Combine them into a MultiPolygon
+        multipolygon = MultiPolygon([polygon1, polygon2])
+
+        data = {
+            'id': [0],
+            'COUNTRY': ["Mock Country"],
+            'geometry': [multipolygon],
+        }
+
+        gdf = gpd.GeoDataFrame(data, crs='EPSG:4326')
+
+        mocker.patch("layers.GADMDownloader.get_shape_data_by_country_name", return_value=gdf)
         adm_area = AdmArea(country="Timor-Leste", level=0)
 
         assert isinstance(adm_area.geometry, MultiPolygon)
+        assert adm_area.geometry == multipolygon
         assert adm_area.adm_name == "Timor-Leste"
 
-    def test_get_country_data_level_1(self, mocker, mock_region_gdf, capsys):
-        mocker.patch("layers.GADMDownloader.get_shape_data_by_country_name", return_value=mock_region_gdf)
+    def test_get_country_data_level_1(self, mocker, capsys):
+        # Create two simple square polygons
+        polygon1 = Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])
+        polygon2 = Polygon([(2, 2), (3, 2), (3, 3), (2, 3)])
+
+        # Combine them into a MultiPolygon
+        multipolygon = MultiPolygon([polygon1, polygon2])
+
+        data = {
+            'id': [0, 1],
+            'COUNTRY': ["Mock Country", "Mock Country"],
+            'NAME_1': ["Mock Region 1", "Mock Region 2"],
+            'geometry': [multipolygon, multipolygon],
+        }
+
+        gdf = gpd.GeoDataFrame(data, crs='EPSG:4326')
+
+        mocker.patch("layers.GADMDownloader.get_shape_data_by_country_name", return_value=gdf)
         AdmArea(country="Timor-Leste", level=1)
 
         printed_output = capsys.readouterr().out.strip().split('\n')
